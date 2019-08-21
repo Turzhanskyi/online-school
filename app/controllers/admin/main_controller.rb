3cbb3635c1a596c82ffa24e970da95a05a571305
Turzhanskyi <turzhansky81@gmail.com>
2019-08-21 22:27:18 +0300

    #3 REST Controller 1 – Онлайн-школа на Ruby on Rails

--- app/controllers/admin/main_controller.rb
+++ app/controllers/admin/main_controller.rb
@@ -1,4 +1,6 @@
 class Admin::MainController < Admin::BaseController
+  skip_before_action :set_active_main_item
+
   def index
   end
 end
---
+++ app/controllers/admin/teachers_controller.rb
@@ -0,0 +1,25 @@
+class Admin::TeachersController < Admin::BaseController
+  def index
+    @teachers = Teacher.order(id: :desc)
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def set_active_main_item
+      @main_menu[:teachers][:active] = true
+    end
+end
